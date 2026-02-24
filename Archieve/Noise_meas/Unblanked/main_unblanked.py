ebe6734f70f1c30ed9161b51cb6d03a9af8d9358
Yuan FANG <yuan.fang@gehealthcare.com>
2026-02-24 18:26:14 +0100

    clean up

--- Archieve/Noise_meas/Unblanked/main_unblanked.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-import sys
-from Noise_meas.Unblanked.core_unblanked.workflow_unblanked import MainWindow
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    w = MainWindow()
-    w.show()
-    sys.exit(app.exec_())
--- Archieve/Output_cond/core/visa_setup.py
+++
@@ -1,5 +0,0 @@
-def connect_instruments():
-    rm = None
-    scope = None
-    swg = None
-    return rm, scope, swg, True
